c111e15dedf69097d5eb7fbf2665073d2d8c9fa9
kevin1688 <jackexi@gmail.com>
2021-10-22 06:45:02 +0800

    Initial Commit

---
+++ classic10/classic10App.swift
@@ -0,0 +1,17 @@
+//
+//  classic10App.swift
+//  classic10
+//
+//  Created by kai wen chen on 2021/10/22.
+//
+
+import SwiftUI
+
+@main
+struct classic10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
