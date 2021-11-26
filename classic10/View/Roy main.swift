133bab17de5103bc2990b652016183201e30e51d
Roy <a0966750212@icloud.com>
2021-11-26 11:05:16 +0800

    Roy main

---
+++ classic10/View/Roy main.swift
@@ -0,0 +1,20 @@
+//
+//  Roy main.swift
+//  classic10
+//
+//  Created by 暨大附中 on 2021/11/26.
+//
+
+import SwiftUI
+
+struct Roy_main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Roy_main_Previews: PreviewProvider {
+    static var previews: some View {
+        Roy_main()
+    }
+}
