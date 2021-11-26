5f6cbd31c87a79efc64231e9117e3e91cee04e7a
lin455 <jack08281113@gmail.com>
2021-11-26 10:59:27 +0800

    Hi1

---
+++ classic10/View/HiView.swift
@@ -0,0 +1,23 @@
+//
+//  HiView.swift
+//  classic10
+//
+//  Created by 暨大附中10 on 2021/11/26.
+//
+
+import SwiftUI
+
+struct HiView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("hh")
+        }
+    }
+}
+
+struct HiView_Previews: PreviewProvider {
+    static var previews: some View {
+        HiView()
+    }
+}
