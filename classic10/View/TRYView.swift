c01fd00a57e70f143eefd8014824a59b4b1d6c40
Dujiashan0711 <shandu7652@gmail.com>
2021-11-26 10:58:33 +0800

    1

---
+++ classic10/View/TRYView.swift
@@ -0,0 +1,20 @@
+//
+//  TRYView.swift
+//  classic10
+//
+//  Created by 暨大附中２ on 2021/11/26.
+//
+
+import SwiftUI
+
+struct TRYView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TRYView_Previews: PreviewProvider {
+    static var previews: some View {
+        TRYView()
+    }
+}
